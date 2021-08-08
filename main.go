e795230160f487ae8c77190cf3f74a5cf07a9305
R. Aidan Campbell <aidan.campbell@gmail.com>
2021-08-07 19:53:48 -0700

    debug profiling endpoint exposed

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"github.com/raidancampbell/pcabinet/conf"
 	"github.com/raidancampbell/pcabinet/tui"
+	"github.com/sirupsen/logrus"
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
 	cfg := conf.Initialize()
+
+	go defaultWebServer()
 
 	//TODO: ServiceSelector should operate on services
 	serviceNames := map[string]string{}
@@ -23,4 +28,9 @@ func main() {
 	//TODO: figure out how to chain together bubbles in bubbletea
 	//TODO: after a value is selected, display a download spinner
 	//TODO: HTTP GET that endpoint, write to some file
+}
+
+// defaultWebServer exists so that I can test it on itself. I don't wanna keep another long-running debug service around.
+func defaultWebServer() {
+	logrus.Fatal(http.ListenAndServe("127.0.0.1:8080", http.DefaultServeMux))
 }
